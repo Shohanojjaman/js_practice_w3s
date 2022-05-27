99204b5eae251fb71ba3c0afe7dfb12a333d9b51
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2022-05-27 20:24:22 +0600

    I am learning js

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
-// document.getElementById("js").innerHTML = 3998 * 10 ;
-
-// document.write(4 * 2);
-
-// alert("It is harmful for you")
-
-function funvar(){
-    var varvalue = 'I am var';
-    var varvalue = 'I am not var'
-    console.log(varvalue)
+function(){
+    let s = 2;
+    s ++;
+    let e = s;
+    console.log(e)
 }
-funvar()   
-
-function fun(){
-    var nai = "ami kew na.";
-    console.log(nai)
-}
-
-fun()
-
-
-function funlet(){
-    let letvalue = 'I am let';
-    letvalue = 'I am not let'
-    console.log (letvalue)
-}
-funlet()   
-
-function funconst(){
-    const constvalue = 'I am const';
-    console.log (constvalue)
-}
-funconst()   
-
-let text1 = "I am leatning ";
-text1+= "JavaScript."
-console.log(text1)
